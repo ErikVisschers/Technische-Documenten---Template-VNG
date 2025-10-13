ca66071ad780a0d143c2937679db4bd702eb658e
ErikVisschers <erik.visschers@smart-ict.com>
2025-10-13 16:29:49 +0200

    Commiting to main to update current state

--- js/config.js
+++ js/config.js
@@ -1,18 +1,26 @@
 var respecConfig = {
-  specStatus: "WV", // Werkversie
-  shortName: "technische-documenten-template",
-  title: "Technische Documentatie Template (VNG ReSpec)",
+  specStatus: "WV", // Werkversie / draft
+  shortName: "technische-documenten-template-test",
+  title: "Testen Technische Documentatie Template (VNG ReSpec)",
   subtitle: "Proof of Concept",
   editors: [
     { name: "Erik Visschers", company: "Smart ICT" }
   ],
-  publishDate: "2025-10-13",
+  authors: [
+    { name: "Erik Visschers", company: "Smart ICT" }
+  ],
   github: {
     repoURL: "https://github.com/ErikVisschers/Technische-Documenten---Template-VNG"
   },
+  publishDate: "2025-10-13",
   content: {
+    // sleutel = markdown-bestandnaam zonder .md
     "abstract": "",
     "ch01": "",
-    "ch02": ""
+    "ch02": "",
+    "VNG-R-Generating": "",
+    "VNG-R-Assembling": "",
+    "VNG-R-Publishing": "",
+    "VNG-R-Foutoplossing": ""
   }
 };
