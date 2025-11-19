9b745845a3c27e9bb183682e5a01de3caf8fa828
ErikVisschers <erik.visschers@smart-ict.com>
2025-11-19 11:37:36 +0100

    Fix respec build + rebuild GH pages pipeline

--- js/config.js
+++ js/config.js
@@ -1,33 +1,23 @@
 var respecConfig = {
-  specStatus: "io", // in ontwikkeling versie / draft
-  edDraftURI: "https://erikvisschers.github.io/Technische-Documenten---Template-VNG/", //aanbevolen door W3C / ReSpec
-  license: "eupl",
-  shortName: "technische-documenten-template-test",
-  title: "Testen van de Technische Documentatie Template (VNG ReSpec)",
-  subtitle: "Proof of Concept",
-  editors: [
-    { name: "Erik Visschers", company: "Smart-ICT" }
-  ],
-  authors: [
-    { name: "Erik Visschers", company: "Smart-ICT" }
-  ],
-  github: {
-    repoURL: "https://github.com/ErikVisschers/Technische-Documenten---Template-VNG"
-  },
-  publishVersion: "2025-10-20",
-  previousPublishVersion: "2025-09-10",
-  publishDate: "2025-10-20",
-  content: {
-    // sleutel = markdown-bestandnaam zonder .md
-    "abstract": "content/abstract.md",
-    "ch01": "content/ch01.md",
-    "ch02": "content/ch02.md",
-    "ch03": "content/ch03.md",
-    // imvertor (example) -> include the imvertor index.html as HTML
-    "imvertor-merge-test": "content/imvertor/20251118-import-logical-name/doc/index.html",
-    "Leon's Chapter 1 - Generating": "content/mermaid1.md",
-    "Leon's Chapter 2 - Assembling": "content/mermaid1.md",
-    "Leon's Chapter 3 - Publishing": "content/mermaid.md",
-    "Leon's Chapter 4 - Foutoplossing": "content/abstract.md"
-  }
+specStatus: "io",
+shortName: "technische-documenten-template-test",
+title: "Testen van de Technische Documentatie Template (VNG ReSpec)",
+editors: [{ name: "Erik Visschers", company: "Smart-ICT" }],
+authors: [{ name: "Erik Visschers", company: "Smart-ICT" }],
+github: { repoURL: "https://github.com/ErikVisschers/Technische-Documenten---Template-VNG" },
+
+
+// keep publish metadata as you need
+publishVersion: "2025-10-20",
+publishDate: "2025-10-20",
+
+
+// Content: keys are section titles (can be any unique keys)
+// values can be string paths or objects { path: '...', class: '...' }
+content: {
+"abstract": "content/abstract.md",
+"ch01": "content/ch01.md",
+"ch02": "content/ch02.md",
+"imvertor-logical": { path: "content/imvertor/doc/index.html", class: "imvertor-section" }
+}
 };
